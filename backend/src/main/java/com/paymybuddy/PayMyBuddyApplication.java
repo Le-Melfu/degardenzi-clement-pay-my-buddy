1a8128d285fc773aa484b134923eebabeacddcc7
Melfu <degardenzi.clement@gmail.com>
2025-08-22 12:45:30 +0200

    cleaning

--- backend/src/main/java/com/paymybuddy/PayMyBuddyApplication.java
+++ backend/src/main/java/com/paymybuddy/PayMyBuddyApplication.java
@@ -1,4 +1,4 @@
-package com.paymybuddycdegardenzi.paymybuddycdegardenzi;
+package com.paymybuddycdegardenzi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
