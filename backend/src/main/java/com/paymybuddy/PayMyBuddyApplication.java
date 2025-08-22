c4e533276c6b31e0908f77ab67c02df586ee6ad0
Melfu <degardenzi.clement@gmail.com>
2025-08-22 14:27:54 +0200

    updated package and gitignore

--- backend/src/main/java/com/paymybuddy/PayMyBuddyApplication.java
+++ backend/src/main/java/com/paymybuddy/PayMyBuddyApplication.java
@@ -1,4 +1,4 @@
-package com.paymybuddycdegardenzi;
+package com.paymybuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
